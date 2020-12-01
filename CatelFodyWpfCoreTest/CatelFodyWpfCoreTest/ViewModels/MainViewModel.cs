7174854f25e625ad525716d11c20decb96b93f1f
Justin Mooney <jumoone@southernco.com>
2020-12-01 16:36:03 -0600

    2 projects
    net framework 4.7.2 showing Catel.Fody namespace is available.
    net core 3.1 showing Catel.Fody namespace is NOT available.

---
+++ CatelFodyWpfCoreTest/CatelFodyWpfCoreTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Catel.Data;
+using Catel.MVVM;
+using System.Threading.Tasks;
+using Catel.Fody;
+/// ^^^^error only in .net core wpf app^^^^^^
+
+namespace CatelFodyWpfCoreTest.ViewModels
+{
+
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+
+        public override string Title { get { return "Main"; } }
+
+        // TODO: Register properties using 'vmprop'
+        // TODO: Register properties that represent models using 'vmpropmodel'
+        // TODO: Register properties that map to models using 'vmpropviewmodeltomodel'
+        // TODO: Register commands using 'vmcommand', 'vmcommandwithcanexecute', 'vmtaskcommand' or 'vmtaskcommandwithcanexecute'
+
+
+        [Model]
+        [Expose("Property")] //error with Catel.Fody in .net core WPF app
+        public string Model { get; set; }
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: Add initialization logic like subscribing to events
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: Add uninitialization logic like unsubscribing from events
+
+            await base.CloseAsync();
+        }
+    }
+}
---
+++ CatelFodyWpfCoreTest/WpfNetFramework_472/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Catel.Fody;
+
+namespace WpfNetFramework_472
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
